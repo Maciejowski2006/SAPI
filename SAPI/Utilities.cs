ca7864335c24f3b896c50cccb669c74b951dac6f
Maciej Chmiel <maciejowski2006yt@gmail.com>
2022-12-06 23:15:40 +0100

    Wrote API & Basic Authorization

--- SAPI/Utilities.cs
+++ SAPI/Utilities.cs
@@ -19,6 +19,8 @@ public enum HttpStatus
 	BadGateway,
 	ServiceUnavailable
 }
+public record BasicAuthCredentials(string username, string password);
+
 public class Utilities
 {
 	private static Dictionary<HttpStatus, string> httpStatusNames = new()
@@ -135,5 +137,64 @@ public class Utilities
 		response.StatusCode = statusCode;
 
 		response.OutputStream.Write(data, 0, data.Length);
+	}
+	
+	/// <summary>
+	/// Checks if user with provided API key exists.
+	/// </summary>
+	/// <param name="keys">List of all API keys authorized</param>
+	/// <param name="headerName">Name of the authorization header(the OpenAPI 3.0 specification says the default should be "X-Api-Key"</param>
+	/// <param name="request">Request ref you got from server - argument in Task()</param>
+	public static bool CheckForKeyAuthorization(List<string> keys, string headerName, ref HttpListenerRequest request)
+	{
+		try
+		{
+			foreach (string key in keys)
+			{
+				if (request.Headers.Get(headerName).Contains(key))
+					return true;
+			}
+			return false;
+		}
+		catch
+		{
+			Console.WriteLine($"Request does not have {headerName}.");
+			return false;
+		}
+	}
+	/// <summary>
+	/// Checks if user with provided credentials exists.
+	/// </summary>
+	/// <param name="credentialsList">List of all usernames and passwords authorized</param>
+	/// <param name="request">Request ref you got from server - argument in Task()</param>
+	public static bool CheckForUserPassAuthorization(List<BasicAuthCredentials> credentialsList, ref HttpListenerRequest request)
+	{
+		try
+		{
+			if (request.Headers.Get("Authorization").Contains("Basic "))
+			{
+				// Get data from header end remove "Basic " at the beginning
+				string authData = request.Headers.GetValues("Authorization").GetValue(0).ToString().Substring(6);
+
+				// Convert from Base64
+				byte[] decodedBase64 = Convert.FromBase64String(authData);
+			
+				// Encode in UTF-8
+				string[] auth = Encoding.UTF8.GetString(decodedBase64).Split(':');
+				BasicAuthCredentials userCredentials = new BasicAuthCredentials(auth[0], auth[1]);
+
+				foreach (BasicAuthCredentials credentials in credentialsList)
+				{
+					if (String.Equals(userCredentials.username, credentials.username) && String.Equals(userCredentials.password, credentials.password))
+						return true;
+				}
+			}
+		}
+		catch
+		{
+			Console.WriteLine($"Request does not have Authorization header.");
+			return false;
+		}
+		return false;
 	}
 }
