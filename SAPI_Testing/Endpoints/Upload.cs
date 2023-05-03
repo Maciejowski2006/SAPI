a4708db0aed1e3707164f40228f6854759df4943
Maciejowski2006 <maciejowski2006yt@gmail.com>
2023-05-03 18:09:14 +0200

    Add file uploads

--- SAPI_Testing/Endpoints/Upload.cs
+++ SAPI_Testing/Endpoints/Upload.cs
@@ -7,115 +7,119 @@ namespace Testing.Endpoints;
 
 public class Upload : IEndpoint
 {
-    public string url { get; } = "upload";
-    public Method method { get; } = Method.POST;
-    public void Task(ref HttpListenerRequest request, ref HttpListenerResponse response, Dictionary<string, string> parameters)
-    {
-        SaveFile(request.ContentEncoding, GetBoundary(request.ContentType), request.InputStream);
+	public string url { get; } = "upload";
+	public Method method { get; } = Method.POST;
+	public void Task(ref HttpListenerRequest request, ref HttpListenerResponse response, Dictionary<string, string> parameters)
+	{
+		SaveFile(request.ContentEncoding, GetBoundary(request.ContentType), request.Headers.Get("File-Name"), request.InputStream);
 
-        Utilities.Error(HttpStatus.NotAcceptable, ref response);
-    }
+		StreamReader r = new(request.InputStream);
+		Console.WriteLine(r.ReadToEnd());
 
-    private static String GetBoundary(String ctype)
-{
-    return "--" + ctype.Split(';')[1].Split('=')[1];
-}
+		Utilities.Error(HttpStatus.NotAcceptable, ref response);
+	}
 
-private static void SaveFile(Encoding enc, String boundary, Stream input)
-{
-    Byte[] boundaryBytes = enc.GetBytes(boundary);
-    Int32 boundaryLen = boundaryBytes.Length;
+	private static String GetBoundary(String ctype)
+	{
+		return "--" + ctype.Split(';')[1].Split('=')[1];
+	}
 
-    using (FileStream output = new FileStream("data", FileMode.Create, FileAccess.Write))
-    {
-        Byte[] buffer = new Byte[1024];
-        Int32 len = input.Read(buffer, 0, 1024);
-        Int32 startPos = -1;
+	private static void SaveFile(Encoding enc, String boundary, string name, Stream input)
+	{
+		Byte[] boundaryBytes = enc.GetBytes(boundary);
+		Int32 boundaryLen = boundaryBytes.Length;
 
-        // Find start boundary
-        while (true)
-        {
-            if (len == 0)
-            {
-                throw new Exception("Start Boundaray Not Found");
-            }
 
-            startPos = IndexOf(buffer, len, boundaryBytes);
-            if (startPos >= 0)
-            {
-                break;
-            }
-            else
-            {
-                Array.Copy(buffer, len - boundaryLen, buffer, 0, boundaryLen);
-                len = input.Read(buffer, boundaryLen, 1024 - boundaryLen);
-            }
-        }
+		using (FileStream output = new FileStream(name, FileMode.Create, FileAccess.Write))
+		{
+			Byte[] buffer = new Byte[1024];
+			Int32 len = input.Read(buffer, 0, 1024);
+			Int32 startPos = -1;
 
-        // Skip four lines (Boundary, Content-Disposition, Content-Type, and a blank)
-        for (Int32 i = 0; i < 4; i++)
-        {
-            while (true)
-            {
-                if (len == 0)
-                {
-                    throw new Exception("Preamble not Found.");
-                }
+			// Find start boundary
+			while (true)
+			{
+				if (len == 0)
+				{
+					throw new Exception("Start Boundary Not Found");
+				}
 
-                startPos = Array.IndexOf(buffer, enc.GetBytes("\n")[0], startPos);
-                if (startPos >= 0)
-                {
-                    startPos++;
-                    break;
-                }
-                else
-                {
-                    len = input.Read(buffer, 0, 1024);
-                }
-            }
-        }
+				startPos = IndexOf(buffer, len, boundaryBytes);
+				if (startPos >= 0)
+				{
+					break;
+				}
+				else
+				{
+					Array.Copy(buffer, len - boundaryLen, buffer, 0, boundaryLen);
+					len = input.Read(buffer, boundaryLen, 1024 - boundaryLen);
+				}
+			}
 
-        Array.Copy(buffer, startPos, buffer, 0, len - startPos);
-        len = len - startPos;
+			// Skip four lines (Boundary, Content-Disposition, Content-Type, and a blank)
+			for (Int32 i = 0; i < 4; i++)
+			{
+				while (true)
+				{
+					if (len == 0)
+					{
+						throw new Exception("Preamble not Found.");
+					}
 
-        while (true)
-        {
-            Int32 endPos = IndexOf(buffer, len, boundaryBytes);
-            if (endPos >= 0)
-            {
-                if (endPos > 0) output.Write(buffer, 0, endPos-2);
-                break;
-            }
-            else if (len <= boundaryLen)
-            {
-                throw new Exception("End Boundaray Not Found");
-            }
-            else
-            {
-                output.Write(buffer, 0, len - boundaryLen);
-                Array.Copy(buffer, len - boundaryLen, buffer, 0, boundaryLen);
-                len = input.Read(buffer, boundaryLen, 1024 - boundaryLen) + boundaryLen;
-            }
-        }
-    }
-}
+					startPos = Array.IndexOf(buffer, enc.GetBytes("\n")[0], startPos);
+					if (startPos >= 0)
+					{
+						startPos++;
+						break;
+					}
+					else
+					{
+						len = input.Read(buffer, 0, 1024);
+					}
+				}
+			}
 
-private static Int32 IndexOf(Byte[] buffer, Int32 len, Byte[] boundaryBytes)
-{
-    for (Int32 i = 0; i <= len - boundaryBytes.Length; i++)
-    {
-        Boolean match = true;
-        for (Int32 j = 0; j < boundaryBytes.Length && match; j++)
-        {
-            match = buffer[i + j] == boundaryBytes[j];
-        }
+			Array.Copy(buffer, startPos, buffer, 0, len - startPos);
+			len = len - startPos;
 
-        if (match)
-        {
-            return i;
-        }
-    }
+			while (true)
+			{
+				Int32 endPos = IndexOf(buffer, len, boundaryBytes);
+				if (endPos >= 0)
+				{
+					if (endPos > 0) output.Write(buffer, 0, endPos - 2);
+					break;
+				}
+				else if (len <= boundaryLen)
+				{
+					throw new Exception("End Boundary Not Found");
+				}
+				else
+				{
+					output.Write(buffer, 0, len - boundaryLen);
+					Array.Copy(buffer, len - boundaryLen, buffer, 0, boundaryLen);
+					len = input.Read(buffer, boundaryLen, 1024 - boundaryLen) + boundaryLen;
+				}
+			}
+		}
+	}
 
-    return -1;
-}
+	private static Int32 IndexOf(Byte[] buffer, Int32 len, Byte[] boundaryBytes)
+	{
+		for (Int32 i = 0; i <= len - boundaryBytes.Length; i++)
+		{
+			Boolean match = true;
+			for (Int32 j = 0; j < boundaryBytes.Length && match; j++)
+			{
+				match = buffer[i + j] == boundaryBytes[j];
+			}
+
+			if (match)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
 }
