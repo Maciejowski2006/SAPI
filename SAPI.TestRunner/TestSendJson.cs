2e45f5bdb2ba53ba9123cf9dcaa328259eadc968
Maciej Chmiel <maciejowski2006yt@gmail.com>
2023-05-20 19:23:31 +0200

    Fixed test

--- SAPI.TestRunner/TestSendJson.cs
+++ SAPI.TestRunner/TestSendJson.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System.Text;
 
 namespace SAPI.TestRunner;
@@ -9,13 +8,13 @@ public class TestSendJson
 	record DataModel(int id, string message);
 	
 	[Fact]
-	public async void SendJson_BodyShouldBe_TheSameAsPostedData()
+	public async void Json_BodyShouldBe_TheSameAsPostedData()
 	{
 		using HttpClient client = new();
 		DataModel data = new DataModel(1, "message");
 		string sentJson = JsonConvert.SerializeObject(data);
 		StringContent sentContent = new (sentJson, Encoding.UTF8, "application/json");
-		HttpResponseMessage response = await client.PostAsync("http://localhost:8000/send-json", sentContent);
+		HttpResponseMessage response = await client.PostAsync("http://localhost:8000/json", sentContent);
 			
 		StreamReader reader = new(await response.Content.ReadAsStreamAsync());
 		string responseContent = await reader.ReadToEndAsync();
