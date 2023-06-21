8c719c29a626b0c005011081e22c76d61f9e2be8
Maciej Chmiel <maciejowski2006yt@gmail.com>
2023-06-21 20:37:47 +0200

    Removed IEndpoint interface

--- SAPI/LLAPI/EndpointManager.cs
+++ SAPI/LLAPI/EndpointManager.cs
@@ -1,23 +1,8 @@
-﻿namespace SAPI.LLAPI
+﻿
+namespace SAPI.LLAPI
 {
 	internal static class EndpointManager
 	{
-		// Only works for IEndpoint interface
-		// public static void FindAndMount(ref List<IEndpoint> endpoints)
-		// {
-		// 	Type type = typeof(IEndpoint);
-		// 	IEnumerable<Type> endpointClasses = AppDomain.CurrentDomain.GetAssemblies()
-		// 		.SelectMany(s => s.GetTypes())
-		// 		.Where(p => type.IsAssignableFrom(p) && !p.IsInterface);
-		//
-		// 	foreach (Type endpointClass in endpointClasses)
-		// 	{
-		// 		IEndpoint endpoint = (Activator.CreateInstance(endpointClass) as IEndpoint)!;
-		// 		endpoints.Add(endpoint);
-		// 	}
-		// }
-		
-		// For Endpoint abstract class
 		public static void FindAndMount(ref List<Endpoint> endpoints)
 		{
 			Type type = typeof(Endpoint);
@@ -28,6 +13,7 @@
 			foreach (Type endpointClass in endpointClasses)
 			{
 				Endpoint endpoint = (Activator.CreateInstance(endpointClass) as Endpoint)!;
+
 				endpoints.Add(endpoint);
 			}
 		}
