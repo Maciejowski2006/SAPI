30b03988c373419d47cdacbcf52c5292c9de8195
Maciej Chmiel <maciejowski2006yt@gmail.com>
2023-07-09 16:03:42 +0200

    Altered Cookie class to give more control over managing cookies

--- SAPI/API/Utilities/Cookies.cs
+++ SAPI/API/Utilities/Cookies.cs
@@ -10,7 +10,7 @@ namespace SAPI.API.Utilities
 		/// <param name="cookieName">Name of the cookie to check</param>
 		/// <param name="cookie">Return Cookie</param>
 		/// <param name="request">Pass from Task()</param>
-		/// <returns>Does cookie exists?</returns>
+		/// <returns>Does cookie exist?</returns>
 		public static bool CheckForCookie(string cookieName, out Cookie? cookie, ref Packet packet)
 		{
 			cookie = packet.Request.Cookies[cookieName];
@@ -24,9 +24,8 @@ namespace SAPI.API.Utilities
 		/// <param name="cookieName">Name of cookie to be saved</param>
 		/// <param name="cookieValue">Value of cookie to be saved</param>
 		/// <param name="response">Pass from Task()</param>
-		public static void GiveCookie(string cookieName, string cookieValue, ref Packet packet)
+		public static void GiveCookie(Cookie cookie, ref Packet packet)
 		{
-			Cookie cookie = new(cookieName, cookieValue);
 			packet.Response.AppendCookie(cookie);
 		}
 	}
